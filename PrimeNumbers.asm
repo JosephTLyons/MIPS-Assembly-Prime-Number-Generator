7359a785e7d71a20be965a24cb144e3ecb4f77cc
Joseph Lyons <josephlyons@josephs-air.pk5001z>
2017-02-21 20:27:43 -0500

    Fixed error that caused the program to keep running (infinite loop) after that 100th prime number was printed:
    My jump cases were weird, I was testing beq and bne right after each other, assuming that checking the second one would be the exact opposite (negation) of the first, but the error in my logic is that flipping just beq to bne doesn't negate, since negating < is actually >=, I wasn't account for the = in the negation, so now, I'm just testing one and jumping directly after (since one test is all that was really needed to begin with)
    
    Fixed some documentation errors
    
    Rotated the registers that was using sgt so I could use set

--- PrimeNumbers.asm
+++ PrimeNumbers.asm
@@ -31,27 +31,27 @@ Loop:       add   $a0, $s0, $zero   # Load number into argument
 
 Skip:       addi  $s0, $s0, 1       # Increment number to test
             bne   $s1, $s2, Loop    # Keep looping until we reach 101 (only testing up to 100 though)
-            beq   $s0, $s1, Exit2   # Exit program
+            j Exit2                 # Exit program
       
 #########################################################      
       
-test_Prime: addi  $t0, $zero, 1     # $t0 = number of divisors for argument, 1 because number always divisible by itself
+test_Prime: addi  $t0, $zero, 1     # $t0 = number of divisors for $a0, 1 because number always divisible by itself
             div   $t1, $a0, 2       # $t1 = number that divides $a0, divided by 2 - we only need to check lower half of numbers
 	    addi, $t3, $zero, 2     # This will be used later on for testing if prime or not
 Loop2:      div   $a0, $t1          # 1st part of modulus
-            mfhi  $t2               # $t1 = number % dividing number
+            mfhi  $t2               # $t2 = number % dividing number
             bne   $t2, $zero, Skip2 # See if argument is evenly divisible by $t1, If so...
             addi  $t0, $t0, 1	    # Increment count of divisors
 Skip2:      sub   $t1, $t1, 1       # Decrement dividing number
 
-  	    sgt   $t4, $t0, $t3     # $t3 = 2, check if count > 2, then number is evenly divisible by more than 2 numbers = not prime
+  	    slt   $t4, $t3, $t0     # $t3 = 2, check if 2 < count, then number is evenly divisible by more than 2 numbers = not prime
             beq   $t4, $zero, Skip3 # Continue looping if prior test was false, otherwise return 0 
             addi  $v0, $zero, 0     # load 0 into $V0
             jr    $ra               # Leave function early with 0 (as false)
       
 Skip3:      slt   $t5, $zero, $t1   # Check to see if dividing number is zero
             beq   $t5, $zero, Exit  # If zero, exit
-            bne   $t5, $zero, Loop2 # Else, run loop again
+            j Loop2                 # Else, run loop again
             
             
 Exit:       slt   $t4, $t0, $t3     # $t3 = 2, check if count < 2, only happens for number "1"
