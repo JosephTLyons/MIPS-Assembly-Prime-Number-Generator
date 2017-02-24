8d9a83d71159fb186b38bc8b83293163caaf7fb0
Joseph Lyons <josephlyons@josephs-air.pk5001z>
2017-02-24 03:28:49 -0500

    Documentation clean up

--- PrimeNumbers.asm
+++ PrimeNumbers.asm
@@ -1,5 +1,5 @@
 # Title: Programming Project Part 2		#Filename: PrimeNumbers.asm
-# Author: Joseph Lyons				#Date: February 15th, 2017
+# Author: Joseph Lyons				    #Date: February 15th, 2017
 # Description: Calculates and displays first 100 prime numbers
 # Input: None
 # Output: 100 Prime Numbers
@@ -11,10 +11,20 @@ outputText:  .asciiz " is prime\n"
 .text
 .globl main
 
-main:
+# Variable Key:
+# $s0 = loop counter and holds nubers to be tested for prime-ness
+# $s1 = Prime number count
+# $s2 = 100: Upper limit of prime numbers
+# $a0 = Number being tested for prime-ness
+# $t0 = Number of divisors for $a0
+# $t1 = Number that divides $a0
+# $t2 = 2: Max number of divisors before a number becomes not prime
+# $t3 = Remainder after dividing, used to test if number is evenly divisble when dividing or not
+# $t4 = Holds 1 or 0 after slt instructions
 
-            addi  $s0, $zero, 1     # $s0 = loop counter and holds numbers to be tested if prime
-            addi  $s1, $zero, 0     # Counts the amount of prime numbers
+main:
+            addi  $s0, $zero, 1     # Set loop counter to 1
+            addi  $s1, $zero, 0     # Set prime number count to 0
             addi  $s2, $zero, 100   # Used to test when we hit 100 prime numbers
 Loop:       add   $a0, $s0, $zero   # Load number into argument
             jal   test_Prime        # Jump to function
@@ -30,22 +40,22 @@ Loop:       add   $a0, $s0, $zero   # Load number into argument
             syscall
 
 Skip:       addi  $s0, $s0, 1       # Increment number to test
-            bne   $s1, $s2, Loop    # Keep looping until we reach 101 (only testing up to 100 though)
+            bne   $s1, $s2, Loop    # Keep looping until we reach 100 prime numbers
             j Exit2                 # Else, exit program
       
 #########################################################      
       
-test_Prime: addi  $t0, $zero, 1     # $t0 = number of divisors for $a0, 1 because number always divisible by itself
-            div   $t1, $a0, 2       # $t1 = number that divides $a0, divided by 2 - we only need to check lower half of numbers
-	    addi, $t3, $zero, 2     # This will be used later on for testing if prime or not
+test_Prime: addi  $t0, $zero, 1     # 1 because number always divisible by itself
+            div   $t1, $a0, 2       # We only need to check lower half of numbers
+            addi, $t2, $zero, 2     # This will be used later on for testing if prime or not
 Loop2:      div   $a0, $t1          # 1st part of modulus
-            mfhi  $t2               # $t2 = number % dividing number
-            bne   $t2, $zero, Skip2 # See if argument is evenly divisible by $t1, If so...
+            mfhi  $t3               # $t3 = number % dividing number
+            bne   $t3, $zero, Skip2 # See if argument is evenly divisible by $t1, If so...
             addi  $t0, $t0, 1	    # Increment count of divisors
 Skip2:      sub   $t1, $t1, 1       # Decrement dividing number
 
-  	    slt   $t4, $t3, $t0     # $t3 = 2, check if 2 < count, then number is evenly divisible by more than 2 numbers = not prime
-            beq   $t4, $zero, Skip3 # Continue looping if prior test was false, otherwise return 0 
+            slt   $t4, $t2, $t0     # Not prime if number of numbers that divide $a0 are > 2
+            beq   $t4, $zero, Skip3 # Continue looping if prior test was false, otherwise return 0
             addi  $v0, $zero, 0     # load 0 into $V0
             jr    $ra               # Leave function early with 0 (as false)
       
@@ -53,15 +63,12 @@ Skip3:      slt   $t5, $zero, $t1   # Check to see if dividing number is zero
             beq   $t5, $zero, Exit  # If zero, exit
             j Loop2                 # Else, run loop again       
             
-Exit:       slt   $t4, $t0, $t3     # $t3 = 2, check if count < 2, only happens for number "1"
+Exit:       slt   $t4, $t0, $t2     # $t2 = 2, check if count < 2, only happens for number "1"
             beq   $t4, $zero, Skip4 # If prior test was false... skip next instructions 
             addi  $v0, $zero, 0     # load 0 into $V0
             jr    $ra               # Leave function early with 0 (as false)
 
 Skip4:      addi  $v0, $zero, 1     # Leave loop, return to main with 1 (as true)
-	    jr    $ra
+            jr    $ra
 	    
 Exit2:
-	    
-     
-      ### Double check that I'm using good register conventions
